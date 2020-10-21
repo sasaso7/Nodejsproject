0bfbc3e97e66c37b91f59c64eb0ca0c3a2c7b52a
sasaso7 <57388672+sasaso7@users.noreply.github.com>
2020-10-21 12:01:35 +0200

    at home

--- sandbox-mobile/src/Screens/AudioDetails.tsx
+++ sandbox-mobile/src/Screens/AudioDetails.tsx
@@ -6,39 +6,34 @@ import { baseUrl } from "../Constants";
 import FileViewer from "react-file-viewer";
 
 export function AudioDetails() {
-  const route = useRoute<RouteProp<ScreenParamList, "AudioDetails">>();
-  const { item } = route.params;
-  const file = `${baseUrl}/${item.source}`;
+    const route = useRoute<RouteProp<ScreenParamList, 'AudioDetails'>>();
+    const { item } = route.params;
 
-  const type = "mp3";
+    return (
+        <SafeAreaView style={styles.screen}>
+            <Text style={styles.title}>{item.title}</Text>
 
-  return (
-    <SafeAreaView style={styles.screen}>
-      <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.desc}>{item.description}</Text>
 
-      <Text style={styles.desc}>{item.description}</Text>
-      <FileViewer fileType={type} filePath={file} />
-    </SafeAreaView>
-  );
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    paddingHorizontal: 16,
-  },
-  title: {
-    fontSize: 20,
-    marginTop: 5,
-    marginBottom: 10,
-    textAlign: "center",
-  },
-  desc: {
-    color: "grey",
-    lineHeight: 20,
-    textAlign: "center",
-  },
-  image: {
-    flex: 1,
-  },
+    screen: {
+        flex: 1,
+        paddingHorizontal: 16,
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 5,
+        marginBottom: 10,
+    },
+    desc: {
+        color: 'grey',
+        lineHeight: 20,
+    },
+    Audio: {
+        flex: 1,
+    },
 });
